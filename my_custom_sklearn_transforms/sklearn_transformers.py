31a3688f0d3e149de11a9e2e955cb5d50b6d25d7
Andre Santos <andre@graodegente.com.br>
2020-08-21 18:52:09 -0300

    Custom transform

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,29 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class StringColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        for col in self.columns:
+            data[col] = data[col].apply(lambda x: str(x) if x == x else "")
+        return data
+
+class MeanColumns(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        data.fillna(data.mean(), inplace=True)
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data
